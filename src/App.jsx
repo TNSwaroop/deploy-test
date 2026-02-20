84a39ff93453a2bcb44361bbcae8a091b2955507
Naga Swaroop <tummalacheruvu.swaroop@gmail.com>
2026-02-20 13:57:09 +0530

    update text

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       <h1>My First React App</h1>
-      <p>I successfully created a React project.</p>
+      <p>I successfully completed critical test.</p>
     </div>
   );
 }
